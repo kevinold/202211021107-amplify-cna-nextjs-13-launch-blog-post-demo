83698ef8c0f192e01bc340941e140a560136d49f
Kevin Old <kevin@kevinold.com>
2022-11-07 14:12:18 -0600

    add amplify storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s3c1d22810": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
