8aa0901c17a89e53fd661c3df3b9a7ed8fb27043
Kevin Old <kevin@kevinold.com>
2022-11-07 12:30:23 -0600

    wip - admin

--- pages/admin.js
+++ pages/admin.js
@@ -1,10 +1,8 @@
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Button, Flex, Heading, TextField, View } from "@aws-amplify/ui-react";
 import { Analytics, API, Auth, withSSRContext } from "aws-amplify";
-import Head from "next/head";
 import React from "react";
 import { createPost } from "../src/graphql/mutations";
 import { listPosts } from "../src/graphql/queries";
-import styles from "../styles/Home.module.css";
 
 export async function getServerSideProps({ req }) {
   const SSR = withSSRContext({ req });
@@ -51,64 +49,36 @@ async function handleCreatePost(event) {
   }
 }
 
-export default function Home({ posts = [], user }) {
+function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Amplify + Next.js</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <View padding="2rem">
+      <Flex direction={"row"}>
+        <Heading level={2}>Blog Admin</Heading>
+        <Button type="button" onClick={() => Auth.signOut()}>
+          Sign out
+        </Button>
+      </Flex>
+      <View as="main" padding="2rem">
+        <form onSubmit={handleCreatePost}>
+          <TextField
+            defaultValue={`Today, ${new Date().toLocaleTimeString()}`}
+            label="Title"
+            errorMessage="There is an error"
+            name="title"
+          />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Amplify + Next.js</h1>
+          <TextField
+            defaultValue="I built an Amplify app with Next.js!"
+            name="content"
+            label="Content"
+            errorMessage="There is an error"
+          />
 
-        <p className={styles.description}>
-          <code data-test="posts-count" className={styles.code}>
-            {posts.length}
-          </code>
-          posts
-        </p>
-
-        <div className={styles.grid}>
-          {posts.map((post) => (
-            <a
-              data-test={`post-${post.id}`}
-              className={styles.card}
-              href={`/posts/${post.id}`}
-              key={post.id}
-            >
-              <h3>{post.title}</h3>
-              <p>{post.content}</p>
-            </a>
-          ))}
-
-          <div className={styles.card}>
-            <h3 className={styles.title}>New Post</h3>
-
-            <Authenticator>
-              <form onSubmit={handleCreatePost}>
-                <fieldset>
-                  <legend>Title</legend>
-                  <input defaultValue={`Today, ${new Date().toLocaleTimeString()}`} name="title" />
-                </fieldset>
-
-                <fieldset>
-                  <legend>Content</legend>
-                  <textarea defaultValue="I built an Amplify app with Next.js!" name="content" />
-                </fieldset>
-
-                <button>Create Post</button>
-                <button type="button" onClick={() => Auth.signOut()}>
-                  Sign out
-                </button>
-              </form>
-              <code>
-                <pre>{JSON.stringify(user, null, 2)}</pre>
-              </code>
-            </Authenticator>
-          </div>
-        </div>
-      </main>
-    </div>
+          <Button>Create Post</Button>
+        </form>
+      </View>
+    </View>
   );
 }
+
+export default withAuthenticator(Home);
