f4292c5f4dc07b08e93bbcb310c21769059c243f
Kevin Old <kevin@kevinold.com>
2022-11-07 15:12:33 -0600

    wip - iterate on admin form

--- pages/admin.js
+++ pages/admin.js
@@ -12,69 +12,84 @@ import {
   View,
   withAuthenticator,
 } from "@aws-amplify/ui-react";
-import { API, Auth, withSSRContext } from "aws-amplify";
-import React from "react";
+import { API, Auth, graphqlOperation, withSSRContext } from "aws-amplify";
+import React, { useEffect, useState } from "react";
 import { createPost, deletePost } from "../src/graphql/mutations";
 import { listPosts } from "../src/graphql/queries";
+import { onCreatePost } from "../src/graphql/subscriptions";
 
 export async function getServerSideProps({ req }) {
   const SSR = withSSRContext({ req });
   const response = await SSR.API.graphql({ query: listPosts });
 
-  let user = null;
-
-  try {
-    user = await SSR.Auth.currentAuthenticatedUser();
-    console.log(user);
-  } catch (e) {}
-
   return {
     props: {
-      posts: response.data.listPosts.items,
+      serverPosts: response.data.listPosts.items,
     },
   };
 }
 
-async function handleCreatePost(event) {
-  event.preventDefault();
+function Home({ serverPosts = [] }) {
+  const [posts, setPosts] = useState(serverPosts);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-  const form = new FormData(event.target);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const result = await API.graphql(graphqlOperation(listPosts));
+      setPosts(result.data.listPosts.items);
+    };
 
-  try {
-    const { data } = await API.graphql({
-      authMode: "AMAZON_COGNITO_USER_POOLS",
-      query: createPost,
-      variables: {
-        input: {
-          title: form.get("title"),
-          content: form.get("content"),
-        },
+    fetchPosts();
+    const createSub = API.graphql(graphqlOperation(onCreatePost)).subscribe({
+      next: ({ value }) => {
+        setPosts((posts) => [...posts, value.data.onCreateTodo]);
       },
     });
-  } catch ({ errors }) {
-    console.error(...errors);
-    throw new Error(errors[0].message);
-  }
-}
 
-async function onDeletePost(id) {
-  try {
-    const { data } = await API.graphql({
-      authMode: "AMAZON_COGNITO_USER_POOLS",
-      query: deletePost,
-      variables: {
-        input: {
-          id,
+    return () => {
+      createSub.unsubscribe();
+    };
+  }, []);
+
+  async function handleCreatePost() {
+    try {
+      const { data } = await API.graphql({
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+        query: createPost,
+        variables: {
+          input: {
+            title,
+            content,
+          },
         },
-      },
-    });
-  } catch ({ errors }) {
-    console.error(...errors);
-    throw new Error(errors[0].message);
-  }
-}
+      });
 
-function Home({ posts = [] }) {
+      setTitle("");
+      setContent("");
+    } catch ({ errors }) {
+      console.error(...errors);
+      throw new Error(errors[0].message);
+    }
+  }
+
+  async function onDeletePost(id) {
+    try {
+      const { data } = await API.graphql({
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+        query: deletePost,
+        variables: {
+          input: {
+            id,
+          },
+        },
+      });
+    } catch ({ errors }) {
+      console.error(...errors);
+      throw new Error(errors[0].message);
+    }
+  }
+
   return (
     <View padding="2rem">
       <Flex direction={"row"}>
@@ -85,22 +100,24 @@ function Home({ posts = [] }) {
       </Flex>
       <View as="main" paddingTop="2rem" width={"50%"}>
         <Heading level={5}>New Post</Heading>
-        <form onSubmit={handleCreatePost}>
+        <form>
           <TextField
-            defaultValue={`Today, ${new Date().toLocaleTimeString()}`}
             label="Title"
             errorMessage="There is an error"
             name="title"
+            onChange={(e) => setTitle(e.current.value)}
           />
 
           <TextField
-            defaultValue="I built an Amplify app with Next.js!"
             name="content"
             label="Content"
             errorMessage="There is an error"
+            onChange={(e) => setContent(e.current.value)}
           />
 
-          <Button marginTop="small">Create Post</Button>
+          <Button marginTop="small" onClick={() => handleCreatePost()}>
+            Create Post
+          </Button>
         </form>
       </View>
       <Divider padding="medium" />
