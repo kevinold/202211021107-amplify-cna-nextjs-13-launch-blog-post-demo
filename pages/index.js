3955ab4210176131bdbcac45325bcbbff7b62bf1
Kevin Old <kevin@kevinold.com>
2022-11-21 15:53:29 -0600

    adapt homepage to be Roadmap

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,29 @@
-import {
-  Card,
-  Collection,
-  Divider,
-  Heading,
-  View,
-} from "@aws-amplify/ui-react";
+import { Card, Collection, Heading, View } from "@aws-amplify/ui-react";
 import { withSSRContext } from "aws-amplify";
-import Link from "next/link";
 import React from "react";
-import { listPosts } from "../src/graphql/queries";
+import { listFeatures } from "../src/graphql/queries";
 
 export async function getServerSideProps({ req }) {
   const SSR = withSSRContext({ req });
-  const response = await SSR.API.graphql({ query: listPosts });
+  const response = await SSR.API.graphql({ query: listFeatures });
 
   return {
     props: {
-      posts: response.data.listPosts.items,
+      posts: response.data.listFeatures.items,
     },
   };
 }
 
-export default function Home({ posts = [] }) {
+export default function Home({ features = [] }) {
   return (
     <View padding="2rem">
-      <Heading level={2}>Blog</Heading>
+      <Heading level={2}>Roadmap</Heading>
       <View as="main" padding="2rem">
-        <Collection items={posts} type="list" gap="20px" wrap="nowrap">
-          {(post, index) => (
+        <Collection items={features} type="list" gap="20px" wrap="nowrap">
+          {(feature, index) => (
             <Card key={index} maxWidth="50rem">
               <View padding="medium">
-                <Link href={`/posts/${post.id}`} key={post.id}>
-                  <Heading padding="medium">{post.title}</Heading>
-                </Link>
-                <Divider padding="xs" />
+                <Heading padding="medium">{feature.title}</Heading>
               </View>
             </Card>
           )}
