45b0c5f8044216e531172831f3579891710d8b98
Kevin Old <kevin@kevinold.com>
2023-03-14 10:06:51 -0500

    configure swr with custom fetcher using amplify API

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,27 @@
 import "@aws-amplify/ui-react/styles.css";
-import { Amplify } from 'aws-amplify';
+import { Amplify, graphqlOperation } from "aws-amplify";
+import React from "react";
 import awsExports from "../src/aws-exports";
-import '../styles/globals.css';
+import "../styles/globals.css";
+
+import { API } from "aws-amplify";
+import { SWRConfig } from "swr";
+
+const fetcher = (query, variables, additionalHeaders) =>
+  API.graphql(graphqlOperation(query, variables, additionalHeaders));
 
 Amplify.configure({ ...awsExports, ssr: true });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig
+      value={{
+        fetcher,
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
